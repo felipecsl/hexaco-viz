95e31d6c74d754e33455aef79e5ba92e3d804a48
Felipe Lima <felipe.lima@gmail.com>
2019-11-12 21:54:40 -0800

    Code nits

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let PDFParser = require("pdf2json"),
   jsonPath = require('jsonpath');
 
-let parsePDF = (path) => {
+let parsePDF = (path, callback) => {
   const pdfParser = new PDFParser();
   pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
   pdfParser.on("pdfParser_dataReady", pdfData => {
     let i = 8;
+    let results = [];
     while (i < 132) {
       let queryAndIncrement = () => {
         let matches = jsonPath.query(pdfData, `$.formImage.Pages[0].Texts[${i++}].R[0].T`);
         return decodeURIComponent(matches[0]);
       };
-      let label = queryAndIncrement();
-      let score = queryAndIncrement();
-      let median = queryAndIncrement();
-      let tenth90th = queryAndIncrement();
-      console.log(`${label}: ${score}, 50th P: ${median}, 10-90th P: ${tenth90th}`);
-      if ((i - 13) % 5 == 0) {
-        console.log('');
-      }
+      results.push({
+        label: queryAndIncrement(),
+        score: queryAndIncrement(),
+        median: queryAndIncrement(),
+        tenth90th: queryAndIncrement()
+      });
     };
+    callback(results);
   });
   pdfParser.loadPDF(path);
 }
@@ -27,5 +27,9 @@ let parsePDF = (path) => {
 if (process.argv.length < 3) {
   console.warn("Please provide the path to the PDF file with the test results.");
 } else {
-  parsePDF(process.argv[2]);
+  parsePDF(process.argv[2], results => {
+    results.forEach(r => {
+      console.log(`${r.label}: ${r.score}, 50th P: ${r.median}, 10-90th P: ${r.tenth90th}`);
+    });
+  });
 }
