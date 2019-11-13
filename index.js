1d04be55748f8027704d13a18efd1732b0b1a014
Felipe Lima <felipe.lima@gmail.com>
2019-11-12 21:45:11 -0800

    DRU

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError
 pdfParser.on("pdfParser_dataReady", pdfData => {
   let i = 8;
   while (i < 132) {
-    let label = decodeURIComponent(jsonPath.query(pdfData, `$.formImage.Pages[0].Texts[${i++}].R[0].T`)[0]);
-    let score = jsonPath.query(pdfData, `$.formImage.Pages[0].Texts[${i++}].R[0].T`)[0];
-    let median = jsonPath.query(pdfData, `$.formImage.Pages[0].Texts[${i++}].R[0].T`)[0];
-    let tenth90th = decodeURIComponent(jsonPath.query(pdfData, `$.formImage.Pages[0].Texts[${i++}].R[0].T`)[0]);
+    let queryAndIncrement = () => {
+      let matches = jsonPath.query(pdfData, `$.formImage.Pages[0].Texts[${i++}].R[0].T`);
+      return decodeURIComponent(matches[0]);
+    };
+    let label = queryAndIncrement();
+    let score = queryAndIncrement();
+    let median = queryAndIncrement();
+    let tenth90th = queryAndIncrement();
     console.log(`${label}: ${score}, 50th P: ${median}, 10-90th P: ${tenth90th}`);
     if ((i - 13) % 5 == 0) {
       console.log('');
