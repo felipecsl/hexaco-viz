872431309da7d7a5ae42ee8c97a71c93d4f9f805
Felipe Lima <felipe.lima@gmail.com>
2019-11-14 21:12:33 -0800

    Display results on screen

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 import pdfjs from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
-import jsonPath from 'jsonpath';
 
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
-let pdfToText = (data) => {
+const pdfToText = (data) => {
   return pdfjs.getDocument(data).promise.then((pdf) => {
     return pdf.getPage(1).then((page) => {
       return page.getTextContent().then((textContent) => {
@@ -16,44 +15,57 @@ let pdfToText = (data) => {
       });
     });
   });
-}
+};
 
-function handleFileChange(files) {
-  const reader = new FileReader();
-  reader.onload = function (e) {
-    pdfToText(e.target.result).then((items) => {
-      let results = [];
-      let i = 0;
-      while (i < items.length) {
-        results.push({
-          label: items[i++],
-          score: items[i++],
-          median: items[i++],
-          tenth90th: items[i++]
-        });
-      }
-      console.log(results);
-    });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [] };
+    this.handleFileChange = this.handleFileChange.bind(this);
   }
-  reader.readAsArrayBuffer(files[0]);
-}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input
-          className="App-link"
-          type="file"
-          id="input"
-          onChange={(e) => handleFileChange(e.target.files)}
-        />
-      </header>
-    </div >
-  );
+  handleFileChange(files) {
+    const reader = new FileReader();
+    let component = this;
+    reader.onload = (e) => {
+      pdfToText(e.target.result).then((items) => {
+        let results = [];
+        let i = 0;
+        while (i < items.length) {
+          results.push({
+            label: items[i++],
+            score: items[i++],
+            median: items[i++],
+            tenth90th: items[i++]
+          });
+        }
+        component.setState({ items: results });
+      });
+    };
+    reader.readAsArrayBuffer(files[0]);
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <input
+            className="App-link"
+            type="file"
+            id="input"
+            onChange={(e) => this.handleFileChange(e.target.files)}
+          />
+          <ul>
+            {this.state.items.map(item => (
+              <li key={item.label}>
+                {item.label}, {item.score}, {item.median}, {item.tenth90th}
+              </li>
+            ))}
+          </ul>
+        </header>
+      </div >
+    );
+  }
 }
 
 export default App;
